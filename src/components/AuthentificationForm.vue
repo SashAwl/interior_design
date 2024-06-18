f9948f390c3ccdc214637e98e5157b01b0e66fc3
SashAwl <aleks.shilova@gmail.com>
2024-06-18 12:27:55 +0300

    created authentfication component

---
+++ src/components/AuthentificationForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h3>Authentification</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AuthentificationForm',
+}
+</script>
+
+<style lang="scss" scoped></style>
